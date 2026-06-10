38aecf9869276b66c9467b204a09e53c4a29f2e7
MeenuPP <meenumohan784@gmail.com>
2026-06-10 15:00:24 +0530

    first commit

---
+++ lib/calculator.dart
@@ -0,0 +1,15 @@
+import 'package:calculator/home.dart';
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+
+class Calculator extends StatelessWidget {
+  const Calculator({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    Future.delayed(Duration(seconds: 3),(){
+      Navigator.push(context, MaterialPageRoute(builder:(context) => Home(),));
+  },);
+    return Scaffold(body: Center(child: Lottie.asset("assets/splash.json"),),);
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,8 @@
+import 'package:calculator/calculator.dart';
+
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(home: Calculator(),));
+}
+
